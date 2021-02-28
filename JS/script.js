f4683a00a1d752e141039608bdd9b54472a460c6
unknown <example@gmail.com>
2021-02-28 17:56:51 +0800

    homework2

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
 const personalMovieDB = {
@@ -8,12 +10,21 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов ?', 'logan'),
-    b = prompt('На сколько оцените его ?', '8.1'),
-    c = prompt('Один из последних просмотренных фильмов ?', 'logan'),
-    d = prompt('На сколько оцените его ?', '8.1');
+// const a = prompt('Один из последних просмотренных фильмов ?', 'logan'),
+//       b = prompt('На сколько оцените его ?', '8.1'),
+//       c = prompt('Один из последних просмотренных фильмов ?', 'logan'),
+//       d = prompt('На сколько оцените его ?', '8.1');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) { // Этот цикл заменил 13-19 строки
+    const a = prompt('Один из последних просмотренных фильмов ?', 'logan'),
+          b = prompt('На сколько оцените его ?', '8.1');
+
+          personalMovieDB.movies[a] = b;
+}
+
+
 
 console.log(personalMovieDB);
